912fc08a16f0508c6d2a1cdadb5dd04e653e8c39
unknown <KJ@KJ-PC.(none)>
2012-04-19 22:29:46 -0400

    Initial commit

---
+++ audioStream.js
@@ -0,0 +1,41 @@
+var audioStream = function(fillBuffer) {
+    var BUFFER_SIZE = 1024;
+    var SAMPLE_RATE = 44100;
+    var frame = 0;
+    var samples = new Float32Array(BUFFER_SIZE);
+
+    if(typeof webkitAudioContext != "undefined") {
+        var context = new webkitAudioContext();
+        var node = context.createJavaScriptNode(BUFFER_SIZE, 1, 1);
+        node.onaudioprocess = function(e) {
+            var left = e.outputBuffer.getChannelData(0), 
+                right = e.outputBuffer.getChannelData(1);
+
+            fillBuffer(frame, samples, BUFFER_SIZE, SAMPLE_RATE);
+            frame += BUFFER_SIZE;
+
+            for(var i = 0; i < BUFFER_SIZE; i++)
+                left[i] = right[i] = samples[i];
+        }
+
+        node.connect(context.destination);
+        //Stop Chrome from throwing node out
+        window["node" + Math.random()] = node;
+    }
+    else {
+        var audio = new Audio();
+        audio.mozSetup(1, SAMPLE_RATE);
+        
+        setInterval(function() {
+            var ahead = frame - audio.mozCurrentSampleOffset();
+
+            //125ms latency is lowest while still stable
+            if(ahead < SAMPLE_RATE / 8) {
+                fillBuffer(frame, samples, BUFFER_SIZE, SAMPLE_RATE);
+                frame += BUFFER_SIZE;
+
+                audio.mozWriteAudio(samples); 
+            }
+        }, 10);
+    }
+}
